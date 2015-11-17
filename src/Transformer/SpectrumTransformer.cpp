34d4b23ab6eee7c4c3351b2a9667a3f01ccdbc01
dpayne <darby.payne@gmail.com>
2015-11-17 01:00:47 -0800

    Adding top bar to visualizer

--- src/Transformer/SpectrumTransformer.cpp
+++ src/Transformer/SpectrumTransformer.cpp
@@ -13,6 +13,11 @@
 #include <iostream>
 #include <stdio.h>
 
+namespace
+{
+static const double k_top_row_decay_rate = 9.5 / 10.0;
+}
+
 vis::SpectrumTransformer::SpectrumTransformer(const Settings *const settings)
     : m_settings{settings}
 {
@@ -91,26 +96,46 @@ void vis::SpectrumTransformer::draw_spectrum(int32_t win_height,
     // cut bandwidth a little to achieve better look
     const auto bins_per_bar =
         (m_fftw_results / static_cast<size_t>(win_width)) * (7.0 / 10);
-    double bar_height;
-    int32_t bar_bound_height;
+    double bar_height, top_row_bar_height;
+    int32_t bar_bound_height, top_row_bar_bound_height;
+    m_freq_magnitudes_top_row.reserve(m_freq_magnitudes.size());
     for (auto column_index = 0; column_index < win_width; ++column_index)
     {
         bar_height = 0;
-        for (auto j = 0; j < bins_per_bar; ++j)
+        top_row_bar_height = 0;
+        for (auto j = 0u; j < bins_per_bar; ++j)
         {
-            bar_height += m_freq_magnitudes[static_cast<size_t>(
-                column_index * bins_per_bar + j)];
+            auto freq_magnitude_index =
+                static_cast<size_t>((column_index * bins_per_bar) + j);
+
+            bar_height += m_freq_magnitudes[freq_magnitude_index];
+            if (j >= m_freq_magnitudes_top_row.size())
+            {
+                top_row_bar_height = m_freq_magnitudes[freq_magnitude_index];
+                m_freq_magnitudes_top_row.push_back(top_row_bar_height);
+            }
+            else
+            {
+                m_freq_magnitudes_top_row[freq_magnitude_index] =
+                    std::max(m_freq_magnitudes_top_row[freq_magnitude_index] *
+                                 k_top_row_decay_rate,
+                             m_freq_magnitudes[freq_magnitude_index]);
+
+                top_row_bar_height +=
+                    m_freq_magnitudes_top_row[freq_magnitude_index];
+            }
         }
 
-        // buff higher frequencies
-        bar_height *= std::log2(2 + column_index) * (100.0 / win_width);
-        // moderately normalize the heights
-        bar_height = std::pow(bar_height, 0.5);
+        top_row_bar_height = std::max(top_row_bar_height, bar_height);
+
+        bar_bound_height = normalizeHeight(win_width, win_height, column_index,
+                                           bins_per_bar, bar_height);
+
+        top_row_bar_bound_height =
+            normalizeHeight(win_width, win_height, column_index, bins_per_bar,
+                            top_row_bar_height);
 
         auto row_index_sign = flipped ? -1 : 1;
-        bar_bound_height = std::min(
-            static_cast<int32_t>(bar_height / bins_per_bar), win_height);
-
         std::wstring msg{m_settings->get_spectrum_character()};
         for (auto row_index = 0; row_index <= bar_bound_height; ++row_index)
         {
@@ -118,7 +143,29 @@ void vis::SpectrumTransformer::draw_spectrum(int32_t win_height,
                           column_index, writer->to_color(row_index, win_height),
                           msg);
         }
+
+        // write the top row element, it will always be colored the max color.
+        writer->write(win_height + (row_index_sign * top_row_bar_bound_height),
+                      column_index,
+                      writer->to_color(top_row_bar_bound_height, win_height),
+                      msg);
     }
+}
+
+int32_t vis::SpectrumTransformer::normalizeHeight(const int32_t win_width,
+                                                  const int32_t win_height,
+                                                  const int32_t column_index,
+                                                  const double bins_per_bar,
+                                                  const double bar_height)
+{
+    // buff higher frequencies
+    double normalized_bar_height =
+        bar_height * (std::log2(2 + column_index) * (100.0 / win_width));
+    // moderately normalize the heights
+    normalized_bar_height = std::pow(normalized_bar_height, 0.5);
+
+    return std::min(static_cast<int32_t>((normalized_bar_height / bins_per_bar)/1.75),
+                    win_height);
 }
 
 void vis::SpectrumTransformer::execute_fftw_plan(int32_t win_height)
