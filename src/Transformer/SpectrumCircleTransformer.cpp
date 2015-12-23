a52f4ee79a792e3842a6f90e5e36971f11e07480
dpayne <darby.payne@gmail.com>
2015-12-22 23:54:33 -0800

    Adding more logic for spectrum circle

--- src/Transformer/SpectrumCircleTransformer.cpp
+++ src/Transformer/SpectrumCircleTransformer.cpp
@@ -14,7 +14,8 @@ namespace
 {
 }
 
-vis::SpectrumCircleTransformer::SpectrumCircleTransformer(const Settings *const settings)
+vis::SpectrumCircleTransformer::SpectrumCircleTransformer(
+    const Settings *const settings)
     : SpectrumTransformer(settings), m_settings{settings}
 {
 }
@@ -23,48 +24,50 @@ vis::SpectrumCircleTransformer::~SpectrumCircleTransformer()
 {
 }
 
-
-void vis::SpectrumCircleTransformer::draw_bars(const std::vector<double> &bars,
-               const std::vector<double> &bars_falloff, int32_t win_height,
-               const bool flipped, const std::wstring &bar_row_msg,
-               vis::NcursesWriter *writer)
+void vis::SpectrumCircleTransformer::draw_bars(
+    const std::vector<double> &bars, const std::vector<double> &bars_falloff,
+    int32_t win_height, const bool flipped, const std::wstring &bar_row_msg,
+    vis::NcursesWriter *writer)
 {
-    auto x = 0.0;
-    auto y = 0.0;
-
-    const auto half_height = win_height / 2;
-    const auto win_width = NcursesUtils::get_window_width();
+    const auto half_height = win_height / 2.0;
+    const auto half_width = NcursesUtils::get_window_width() / 2.0;
 
     m_settings->get_fps();
 
-    for (auto i = 0u; i < std::min(bars.size(), bars_falloff.size()); ++i)
+    //TODO: add const everywhere and use min of half height or half width
+    const auto max_distance = std::sqrt(std::pow(half_height, 2) + std::pow(half_height, 2));
+
+    for (auto i = 0u; i < bars.size(); ++i)
     {
-        auto distance =
-            std::sqrt(std::pow(bars[i], 2) +
-                      std::pow(0.0, 2));
-        ColorIndex color_distance = writer->to_color_pair(
-            static_cast<int32_t>(distance), 16);
+        auto rad =
+            (static_cast<double>(i) / static_cast<double>(win_height)) * 360.0;
 
-        const auto scaling_multiplier =
-            1.25 * (half_height);
-
-        // Rotate around X and Y axis.
-        x = x * scaling_multiplier;
-        y = y * scaling_multiplier;
-
-        for ( auto row_index = 0u; row_index < bars[i]; ++row_index)
+        auto bar_height = std::max(bars_falloff[i], (half_height/2.0));
+        for (auto row_index = 0u; row_index < bar_height; ++row_index)
         {
-            if ( flipped )
+            auto x =
+                static_cast<int32_t>(std::cos(rad) * row_index);
+
+            auto y = static_cast<int32_t>(std::sin(rad) * row_index);
+
+            const auto distance = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+
+            //TODO: precompute colors
+            const ColorIndex color_distance =
+                writer->to_color_pair(static_cast<int32_t>(distance), static_cast<int32_t>(max_distance));
+
+            x += half_height;
+            y += half_width;
+
+
+            //TODO: if stereo just use 180 degrees
+            if (flipped)
             {
-                write(static_cast<int32_t>(half_height + y),
-                              static_cast<int32_t>(x + win_width / 2.0),
-                              color_distance, bar_row_msg, writer);
+                write(x, y, color_distance, bar_row_msg, writer);
             }
             else
             {
-                write(static_cast<int32_t>(half_height + y),
-                              static_cast<int32_t>(x + win_width / 2.0),
-                              color_distance, bar_row_msg, writer);
+                write(x, y, color_distance, bar_row_msg, writer);
             }
         }
     }
