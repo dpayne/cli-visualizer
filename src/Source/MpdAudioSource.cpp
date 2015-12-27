3369916c51529f184186b568acf3f6fa447baf70
dpayne <darby.payne@gmail.com>
2015-12-26 17:35:54 -0700

    Fixed formatting. Fixed reading mpd file so that it is non-blocking.
    Former-commit-id: 45bca420a8869c039cddd5d0cd39ce578090a2fe

--- src/Source/MpdAudioSource.cpp
+++ src/Source/MpdAudioSource.cpp
@@ -8,45 +8,115 @@
 #include "Source/MpdAudioSource.h"
 #include "Utils/Logger.h"
 #include <string.h>
+#include <iostream>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+namespace
+{
+static const int32_t k_read_attempts = 100;
+static const int64_t k_read_attempt_sleep_interval_nanosecs =
+    1L * 1000000L; // 1 millisecond
+static struct timespec k_read_attempt_sleep_timespec = {
+    0, k_read_attempt_sleep_interval_nanosecs};
+}
 
 vis::MpdAudioSource::MpdAudioSource(const Settings *const settings)
     : m_settings{settings}
 {
-    // open mpd file
-    m_fifo_stream.open(m_settings->get_mpd_fifo_path(), std::ifstream::in);
+    open_mpd_fifo();
 }
 
-bool vis::MpdAudioSource::read(pcm_stereo_sample *buffer, uint32_t buffer_size)
+bool vis::MpdAudioSource::open_mpd_fifo()
+{
+    m_mpd_fifo_fd = ::open(m_settings->get_mpd_fifo_path().c_str(), O_RDONLY);
+
+    if (m_mpd_fifo_fd < 0)
+    {
+        VIS_LOG(vis::LogLevel::WARN, "Error reading file: %s", strerror(errno));
+        m_mpd_fifo_fd = -1;
+        return false;
+    }
+
+    auto flags = fcntl(m_mpd_fifo_fd, F_GETFL, 0);
+    fcntl(m_mpd_fifo_fd, F_SETFL, flags | O_NONBLOCK);
+
+    return true;
+}
+
+bool vis::MpdAudioSource::read(pcm_stereo_sample *buffer,
+                               const uint32_t buffer_size)
 {
     // try to re-open the stream if it has been closed
-    if (!m_fifo_stream.is_open())
+    if (m_mpd_fifo_fd < 0)
     {
-        m_fifo_stream.open(m_settings->get_mpd_fifo_path(), std::ifstream::in);
+        open_mpd_fifo();
     }
 
-    if (m_fifo_stream.is_open() && m_fifo_stream.good() && !m_fifo_stream.eof())
-    {
-        // read buffer
-        m_fifo_stream.read(reinterpret_cast<char *>(buffer),
-                           buffer_size * sizeof(pcm_stereo_sample));
+    size_t buffer_size_bytes =
+        static_cast<size_t>(sizeof(pcm_stereo_sample) * buffer_size);
+    size_t bytes_left = buffer_size_bytes;
 
-        // Could not read the buffer size, so zero out memory
-        if (m_fifo_stream.fail())
+    if (m_mpd_fifo_fd >= 0)
+    {
+        auto attempts = 0;
+        while (bytes_left > 0)
         {
-            // zero out buffer
-            memset(buffer, 0,
-                   sizeof(pcm_stereo_sample) * m_settings->get_sample_size());
+            // Read buffer
+            int64_t bytes_read = ::read(m_mpd_fifo_fd, buffer, bytes_left);
+
+            // No bytes left
+            if (bytes_read == 0)
+            {
+                VIS_LOG(vis::LogLevel::WARN, "Could not read any bytes");
+                return false;
+            }
+            // Error reading file. Since non-blocking is set, it's possible
+            // there's not enough data yet
+            else if (bytes_read == -1)
+            {
+                auto error_code = errno;
+
+                // EAGAIN means data is not ready yet
+                if (error_code == EAGAIN)
+                {
+
+                    // Try up to k_read_attempts before quiting
+                    if (attempts > k_read_attempts)
+                    {
+                        VIS_LOG(vis::LogLevel::WARN,
+                                "Could not finish reading "
+                                "buffer, bytes read: %d    "
+                                "buffer size: ",
+                                bytes_read, buffer_size_bytes);
+
+                        // zero out buffer
+                        memset(buffer, 0, buffer_size_bytes);
+                        ::close(m_mpd_fifo_fd);
+                        m_mpd_fifo_fd = -1;
+                        return false;
+                    }
+
+                    nanosleep(&k_read_attempt_sleep_timespec, NULL);
+                    ++attempts;
+                }
+                else
+                {
+                    VIS_LOG(vis::LogLevel::WARN, "Error reading file: %d %s",
+                            error_code, strerror(error_code));
+                }
+            }
+            // Bytes were read fine, continue until buffer is full
+            else
+            {
+                bytes_left -= static_cast<size_t>(bytes_read);
+            }
         }
 
-        if (m_fifo_stream.good() && !m_fifo_stream.eof())
-        {
-            return true;
-        }
-    }
-    else
-    {
-        // close the stream and try re-opening on next run
-        m_fifo_stream.close();
+        // Success fully read entire buffer
+        return true;
     }
 
     return false;
@@ -54,8 +124,8 @@ bool vis::MpdAudioSource::read(pcm_stereo_sample *buffer, uint32_t buffer_size)
 
 vis::MpdAudioSource::~MpdAudioSource()
 {
-    if (m_fifo_stream.is_open())
+    if (m_mpd_fifo_fd >= 0)
     {
-        m_fifo_stream.close();
+        ::close(m_mpd_fifo_fd);
     }
 }
